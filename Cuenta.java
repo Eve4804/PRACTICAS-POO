b2b56ddc6ef75836a2f5a8a8984f951648f1d3fe
Eve4804 <143903874+Eve4804@users.noreply.github.com>
2024-01-30 15:02:43 -0600

    Update Cuenta.java

--- Cuenta.java
+++ Cuenta.java
@@ -9,5 +9,41 @@ package tema_1_2;
  * @author Usuario
  */
 public class Cuenta {
+    private String titular;
+    private double cantidad;
+    //constructor predeterminado
+
+    public Cuenta() {
+        this.titular = "";
+        this.cantidad = 0;
+
+    }
+
+    //por cada atributo se debe de poner un set (asigna valor con void) y get (el que regresa valor con el tipo de dato)
+    public Cuenta(String t) {
+        this.titular = t;
+    }
+
+    private void setTitular(String nombre) {
+        this.titular = nombre;
+    }
+
+    private String getTitular() {
+        return this.titular;
+    }
+
+    private void setCantidad(double c) {
+        this.cantidad = c;
+    }
+
+    private double getCantidad() {
+        return this.cantidad;
+    }
     
+    private void ingresar (double cantidad){
+        /*se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se hara nada*/
+        if (cantidad > 0){
+            this.cantidad += cantidad;
+        }
+    }
 }
